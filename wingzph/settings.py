370a88f0c301e4752eff92073be400f1ecc47220
Ronald Tagolino <tags0516@gmail.com>
2025-05-07 12:45:43 +0800

    Add default pagination and ordering class. Update default authentication for checking Admin role.

--- wingzph/settings.py
+++ wingzph/settings.py
@@ -107,7 +107,13 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        "wingzph.permissions.isAdminRole",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
     ]
 }
 
